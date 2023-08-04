3628f6976eb318655308c9bf591bd927bc8c8e12
Aniebiet Francis <francisaniebiet678@gmail.com>
2023-08-04 09:53:40 +0100

    second commit

--- js/color.js
+++ js/color.js
@@ -1,14 +1,29 @@
 const colors = ["green", "red", "blue", "yellow", "orange", "purple", "pink", "black", "white", "violet", "royalblue", "brown", "gray", "gold", "silver", "indigo", "teal", "navy", "maroon", "lime", "olive", "aqua", "teal", "aquamarine", "coral", "crimson", "fuchsia", "lavender", "plum", "salmon", "sandybrown", "seagreen", "sienna", "tan", "tomato", "turquoise", "wheat", "yellowgreen"];
-const bright = ["green", "red", "blue", "yellow", "orange", "purple", "pink", "black", "white", "violet", "royalblue", "gold", "silver", "brown", "gray", "aqua", "teal", "navy", "maroon", "lime", "olive", "aquamarine", "coral", "crimson", "fuchsia", "lavender", "plum", "salmon", "sandybrown", "seagreen", "sienna", "tan", "tomato", "turquoise", "wheat", "yellowgreen"];
 
-let flipButton = document.querySelector('.btn');
+let hamTweet = document.getElementById('ham');
+let open = document.querySelector('.nav-holder')
 let bgColor = document.querySelector(".color");
 
+hamTweet.addEventListener('click', openHamburger)
 
-flipButton.addEventListener('click', () => {
+setInterval(bgChange, 1000);
+
+function bgChange(){
     let randomNumber = Math.floor(Math.random() * colors.length);
-    bgColor.innerHTML = colors[randomNumber];
-    document.body.style.backgroundColor = colors[randomNumber];
-})
+    let randomColor = colors[randomNumber];
+    bgColor.innerHTML = randomColor;
+    document.body.style.backgroundColor = randomColor;
+
+    if (randomColor === "black"){
+        document.body.style.color = "white"
+    } else {
+        document.body.style.color = "black"
+    }
+}
+
+function openHamburger(){
+    open.classList.toggle('hidden');
+}
+
 
 
