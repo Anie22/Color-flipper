742023c109953504af703837612a63453af82fb1
Aniebiet Francis <francisaniebiet678@gmail.com>
2023-08-04 10:27:29 +0100

    second commit

--- js/hex.js
+++ js/hex.js
@@ -2,6 +2,7 @@ const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f'];
 
 // const btn = document.getElementById('btn');
 let hamTweet = document.getElementById('ham');
+let open = document.querySelector('.nav-holder')
 const color = document.querySelector('.color');
 
 hamTweet.addEventListener('click', openHamburger)
