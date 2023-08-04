da0eaffdc6a8e5174793e5a4d4e4c8b62ad4707a
Aniebiet Francis <francisaniebiet678@gmail.com>
2023-08-04 10:09:20 +0100

    second commit

--- js/hex.js
+++ js/hex.js
@@ -1,9 +1,13 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
 
-const btn = document.getElementById('btn');
+// const btn = document.getElementById('btn');
 const color = document.querySelector('.color');
 
-btn.addEventListener('click', () => {
+hamTweet.addEventListener('click', openHamburger)
+
+setInterval(bgChange, 1000);
+
+function bgChange() {
     let hexColor = '#';
 
     for(let i=0; i < 6; i++){
@@ -11,8 +15,18 @@ btn.addEventListener('click', () => {
     }
     color.textContent = hexColor;
     document.body.style.backgroundColor = hexColor;
-});
+
+    if (randomColor === "#000000"){
+        document.body.style.color = "#ffffff"
+    } else {
+        document.body.style.color = "#000000"
+    }
+};
 
 function getRandomNum( array) {
     return Math.floor(Math.random() * array.length)
+}
+
+function openHamburger(){
+    open.classList.toggle('hidden');
 }
